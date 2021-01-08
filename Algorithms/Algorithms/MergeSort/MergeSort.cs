3973037016a3b1e31b5fe6b562c2a1b8b220bce5
Rajeesh Raveendran <rajeesh.raveendran@happiestminds.com>
2021-01-08 14:28:09 +0530

    merge sort added

---
+++ Algorithms/Algorithms/MergeSort/MergeSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public class MergeSort
+    {
+        public static void MergeSortWithTwoArray()
+        {
+            int[] A = new int[] { 2, 5, 6, 8 };
+            int[] B = new int[] { 10, 12, 22, 35 };
+
+            int lenthOfArrayA = A.Length;
+            int lenthOfArrayB = B.Length;
+
+            int i = 0;
+            int j = 0; 
+            int k = 0;
+
+            //Auxiliary array
+            int[] C = new int[lenthOfArrayA + lenthOfArrayB];
+
+            while (i < lenthOfArrayA && j < lenthOfArrayB)
+            {
+                if (A[i] < B[j])
+                {
+                    C[k++] = A[i++];
+                }
+                else
+                {
+                    C[k++] = B[j++];
+                }
+            }
+
+            for (; i < lenthOfArrayA; i++)
+            {
+                C[k++] = A[i];
+            }
+
+            for (; j < lenthOfArrayB; j++)
+            {
+                C[k++] = B[j];
+            }
+
+            Console.WriteLine($"Merge Sorting using two arrays : [{string.Join(",", C)}]");
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithms!");
+            #region Merge Sorting
+            MergeSort.MergeSortWithTwoArray();
+            #endregion
+
+            Console.ReadLine();
+
+        }
+    }
+}
